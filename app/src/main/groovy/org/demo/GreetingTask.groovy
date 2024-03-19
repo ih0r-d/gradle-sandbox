aa755cc90e4e50d769b92ed49796d3e4570eb0fd
Ihor Didyk <ihordidyk95@gmail.com>
2024-03-19 08:27:57 +0200

    draft: add custom task via classes

---
+++ app/src/main/groovy/org/demo/GreetingTask.groovy
@@ -0,0 +1,14 @@
+//import org.gradle.api.DefaultTask
+//import org.gradle.api.tasks.TaskAction
+//import org.gradle.api.tasks.Input
+//
+//class GreetingTask extends DefaultTask {
+//
+//    @Input
+//    String greeting = 'hello from GreetingTask'
+//
+//    @TaskAction
+//    def greet() {
+//        println greeting
+//    }
+//}
